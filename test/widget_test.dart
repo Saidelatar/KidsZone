996854acc8718ae6f519db978f52499e22e6b1ad
Ahmednabil02 <AhmedNabil72@outlook.com>
2024-04-20 00:59:53 +0200

    new change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const KidsZone());
-    // change2
+    //  new change
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
